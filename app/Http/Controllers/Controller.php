46f55dbe6dea9359dd7eebeb4da5cbf73e7ee798
brobert <robert.blonski+git@gmail.com>
2016-06-12 23:58:15 +0200

    dodanie funkcji renderującej zmiana routingu

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,59 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
-class Controller extends BaseController
-{
+use Illuminate\Http\Request;
+
+class Controller extends BaseController {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    protected $base = false;
+
+    protected $view = false;
+
+    protected $data = [];
+
+    public function __construct() {
+
+    }
+
+    protected function set_view( $view_path )
+    {
+        if ( $this->base )
+        {
+            $view_path = $this->base . '.' . $view_path;
+        }
+
+        if ( ! view()->exists( $view_path ) ) {
+            throw new \InvalidArgumentException('View not found: ' . $view_path );
+        }
+        $this->view = $view_path;
+        return $this;
+    }
+
+    protected function set_data( $key, $data ) {
+        $this->data[$key] = $data;
+    }
+
+    protected function render( Request $request ) {
+
+        switch ( $request->format()) {
+            case 'html':
+                return $this->render_html();
+                break;
+            case 'json':
+                return $this->render_json();
+                break;
+        }
+
+    }
+
+    private function render_html() {
+        return view( $this->view, $this->data );
+    }
+
+    private function render_json() {
+        return [
+            'data' => $this->data,
+        ];
+    }
 }
